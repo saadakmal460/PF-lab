cce69aabbb2cdf11905954a2d79c9e6b182df655
Saad@DESKTOP-UBGD9AO <saadakmal460@gmail.comgit config --global user.email saadakmal460@gmail.com>
2022-12-13 19:31:19 +0500

    lab_codes

---
+++ crtask.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+main(){
+
+
+cout << "              ___       ___   "  <<endl;
+cout << "             /  \\~~~~~/   \\  " <<endl;
+cout << "     ,_ _ _ (              )  " <<endl;
+cout << "    /       \\     .  .     /    " <<endl;
+cout << "   /           ___       ___      " <<endl;
+cout << "  /|            (\\     |(      "<<endl;
+cout << "  ^ \\    /____\\    \\    |       " <<endl;
+cout << "     |___|     |___| --*        " <<endl;
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ lab task.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+main(){
+
+cout << "My name is Saad" <<endl;
+cout << "My roll number is 20" <<endl;
+cout << "My section is C" <<endl;
+
+
+
+cout << "****************************************************" <<endl;
+
+
+
+
+
+}
